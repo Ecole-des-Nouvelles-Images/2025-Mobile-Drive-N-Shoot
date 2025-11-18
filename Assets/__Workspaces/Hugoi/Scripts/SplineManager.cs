79ea6e164c737efe0b410cafb1cf8d189e540c34
HIsambart <isambart.hugo0912@gmail.com>
2025-11-18 16:49:55 +0100

    Added SplineManager

--- Assets/__Workspaces/Hugoi/Scripts/SplineManager.cs
+++ Assets/__Workspaces/Hugoi/Scripts/SplineManager.cs
@@ -6,16 +6,40 @@ namespace __Workspaces.Hugoi.Scripts
     [RequireComponent( typeof(SplineContainer))]
     public class SplineManager : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
+        public SplineContainer SplineContainer;
+
+        public int KnotCount;
+        public int TerrainSize;
         
+        private void OnEnable()
+        {
+            SplineContainer = GetComponent<SplineContainer>();
         }
 
-        // Update is called once per frame
-        void Update()
+        [ContextMenu("GenerateSpline")]
+        private void GenerateSpline()
         {
-        
+            SplineContainer.Spline.Clear();
+
+            float space = TerrainSize / (KnotCount - 1);
+            for (float i = 0; i <= TerrainSize; i += space)
+            {
+                Vector3 newPos = new Vector3();
+                if (i == 0 || i == TerrainSize)
+                {
+                    newPos = new Vector3(0, 0, i);
+                }
+                else
+                {
+                    newPos = new Vector3(Random.Range(-40, 40), 0, i);
+                }
+
+                Vector3 tangentIn = new Vector3(0, 0, -5);
+                Vector3 tangentOut = new Vector3(0, 0, 5);
+                
+                BezierKnot newBezierKnot = new BezierKnot(newPos, tangentIn, tangentOut);
+                SplineContainer.Spline.Add(newBezierKnot);
+            }
         }
     }
 }
