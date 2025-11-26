27bebd22014965d6d4f74ad9bbe60595db682eda
HIsambart <isambart.hugo0912@gmail.com>
2025-11-26 12:34:14 +0100

    Added item chance rate spawn

--- Assets/Scripts/MapGeneration/MapManager.cs
+++ Assets/Scripts/MapGeneration/MapManager.cs
@@ -8,6 +8,7 @@ namespace MapGeneration
     {
         [Header("Settings")]
         [SerializeField] private int _checkPointFrequency;
+        [SerializeField] [Range(0f, 1f)] private float _itemChanceRate;
         
         [Header("Prefabs")]
         [SerializeField] private GameObject _mapModulePrefab;
@@ -26,18 +27,32 @@ namespace MapGeneration
         public void SpawnMapModule()
         {
             Vector3 lastPos = _mapModules.Last().transform.position;
-            
-            GameObject newModule = Instantiate(_mapModulePrefab, lastPos + new Vector3(0, 0, 100), Quaternion.identity, transform);
+
+            GameObject newModule;
+            bool haveCheckPoint;
+
             if (_lastCheckPoint % _checkPointFrequency == 0)
             {
-                newModule.GetComponent<MapModuleHandler>().Setup(this, true);
+                haveCheckPoint = true;
                 _lastCheckPoint = 1;
             }
             else
             {
-                newModule.GetComponent<MapModuleHandler>().Setup(this, false);
+                haveCheckPoint = false;
                 _lastCheckPoint++;
             }
+            
+            if (Random.Range(0, 1) > _itemChanceRate)
+            {
+                newModule = Instantiate(_mapModulePrefab, lastPos + new Vector3(0, 0, 100), Quaternion.identity, transform);
+            }
+            else
+            {
+                // FAIRE SPAWN UN MAPMODULEPREFAB AVEC ITEM
+                newModule = Instantiate(_mapModulePrefab, lastPos + new Vector3(0, 0, 100), Quaternion.identity, transform);
+            }
+            
+            newModule.GetComponent<MapModuleHandler>().Setup(this, haveCheckPoint);
             
             _mapModules.Enqueue(newModule);
 
