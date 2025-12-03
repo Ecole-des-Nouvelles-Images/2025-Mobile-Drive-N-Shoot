bf32d0d68fa320d33866080bf7cac3d287b16aa5
HIsambart <isambart.hugo0912@gmail.com>
2025-12-03 12:05:51 +0100

    Added PropsHandler

---
+++ Assets/__Workspaces/Hugoi/Scripts/PropsHandler.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace __Workspaces.Hugoi.Scripts
+{
+    [RequireComponent(typeof(SphereCollider))]
+    public class PropsHandler : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private GameObject _visual;
+        [SerializeField] private ParticleSystem _psDestroy;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                if (_psDestroy) _psDestroy.Play();
+                _visual.SetActive(false);
+                Destroy(gameObject, 2f);
+            }
+        }
+    }
+}
