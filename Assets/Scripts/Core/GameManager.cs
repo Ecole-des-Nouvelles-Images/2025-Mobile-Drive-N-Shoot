65c5f9adb15ec31a97b5d6600335369ea6483af7
HIsambart <isambart.hugo0912@gmail.com>
2025-12-09 15:18:15 +0100

    Added reset materials

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -16,16 +16,28 @@ namespace Core
         private void OnEnable()
         {
             EventBus.OnLoadScene += LoadScene;
+            SceneManager.sceneLoaded += SceneManagerOnsceneLoaded;
+        }
+        
+        private void LoadScene(int index)
+        {
+            SceneManager.LoadScene(index);
+        }
+
+        private void SceneManagerOnsceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+            CurrentCarMaterials[0].SetFloat("_ResistanceProgress", 0f);
+            CurrentTurretMaterials[0].SetFloat("_ResistanceProgress", 0f);
+            CurrentIemExhaustPipeMaterials[0].SetFloat("_ResistanceProgress", 0f);
+            CurrentCarMaterials[0].SetFloat("_HitProgress", 0f);
+            CurrentTurretMaterials[0].SetFloat("_HitProgress", 0f);
+            CurrentIemExhaustPipeMaterials[0].SetFloat("_HitProgress", 0f);
         }
 
         private void OnDisable()
         {
             EventBus.OnLoadScene -= LoadScene;
-        }
-
-        private void LoadScene(int index)
-        {
-            SceneManager.LoadScene(index);
+            SceneManager.sceneLoaded -= SceneManagerOnsceneLoaded;
         }
     }
 }
