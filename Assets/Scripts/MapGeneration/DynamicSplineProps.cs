cc38d53ad2bca7793894d8dce586b929e864ca36
HIsambart <isambart.hugo0912@gmail.com>
2025-12-12 11:55:00 +0100

    Added TypeSpawningProps

--- Assets/Scripts/MapGeneration/DynamicSplineProps.cs
+++ Assets/Scripts/MapGeneration/DynamicSplineProps.cs
@@ -10,6 +10,7 @@ namespace MapGeneration
     {
         [Header("Settings")]
         [SerializeField] private uint _seed;
+        [SerializeField] private TypeSpawningProps _typeSpawning;
         [SerializeField] [Min(2)] private int _density = 50;
         [SerializeField] [Min(0.01f)] private float _width = 2f;
         [SerializeField] private bool _localHeightOffset;
@@ -59,36 +60,85 @@ namespace MapGeneration
                 {
                     float t = j * step;
                     _splineContainer.Evaluate(i, t, out float3 pos, out float3 tan, out float3 up);
-
+                    
                     Vector3 position = pos;
                     Vector3 forward = math.normalize(tan);
                     Vector3 right = Vector3.Cross(up, forward).normalized;
 
-                    Vector3 leftPos = position - right * (_width * 0.5f);
-                    Vector3 rightPos = position + right * (_width * 0.5f);
-
-                    leftPos = transform.InverseTransformPoint(leftPos);
-                    rightPos = transform.InverseTransformPoint(rightPos);
-
-                    leftPos += new Vector3(_random.NextFloat(-_positionOffset, _positionOffset) + transform.position.x, _heightOffset, _random.NextFloat(-_positionOffset, _positionOffset) + transform.position.z);
-                    rightPos += new Vector3(_random.NextFloat(-_positionOffset, _positionOffset) + transform.position.x, _heightOffset, _random.NextFloat(-_positionOffset, _positionOffset) + transform.position.z);
-
-                    if (!_localHeightOffset)
+                    int randomSide = 0;
+                    if (_typeSpawning == TypeSpawningProps.OneSideRandom)
                     {
-                        leftPos.y = _heightOffset;
-                        rightPos.y = _heightOffset;
+                        randomSide = _random.NextInt(1, 3);
                     }
                     
-                    GameObject leftObj = Instantiate(_props[_random.NextInt(0, _props.Count)], leftPos, Quaternion.Euler(0, _random.NextFloat(-_rotationOffset, _rotationOffset), 0), _transformParent);
-                    GameObject rightObj = Instantiate(_props[_random.NextInt(0, _props.Count)], rightPos, Quaternion.Euler(0, _random.NextFloat(-_rotationOffset, _rotationOffset), 0), _transformParent);
+                    if (_typeSpawning == TypeSpawningProps.Symmetrical)
+                    {
+                        Vector3 leftPos = position - right * (_width * 0.5f);
+                        Vector3 rightPos = position + right * (_width * 0.5f);
+
+                        leftPos = transform.InverseTransformPoint(leftPos);
+                        rightPos = transform.InverseTransformPoint(rightPos);
+
+                        leftPos += new Vector3(_random.NextFloat(-_positionOffset, _positionOffset) + transform.position.x, _heightOffset, _random.NextFloat(-_positionOffset, _positionOffset) + transform.position.z);
+                        rightPos += new Vector3(_random.NextFloat(-_positionOffset, _positionOffset) + transform.position.x, _heightOffset, _random.NextFloat(-_positionOffset, _positionOffset) + transform.position.z);
+
+                        if (!_localHeightOffset)
+                        {
+                            leftPos.y = _heightOffset;
+                            rightPos.y = _heightOffset;
+                        }
+                    
+                        GameObject leftObj = Instantiate(_props[_random.NextInt(0, _props.Count)], leftPos, Quaternion.Euler(0, _random.NextFloat(-_rotationOffset, _rotationOffset), 0), _transformParent);
+                        GameObject rightObj = Instantiate(_props[_random.NextInt(0, _props.Count)], rightPos, Quaternion.Euler(0, _random.NextFloat(-_rotationOffset, _rotationOffset), 0), _transformParent);
                 
-                    float leftScaleModifier = _random.NextFloat(_scaleOffsetMinMax.x, _scaleOffsetMinMax.y);
-                    float rightScaleModifier = _random.NextFloat(_scaleOffsetMinMax.x, _scaleOffsetMinMax.y);
-                    leftObj.transform.localScale *= leftScaleModifier;
-                    rightObj.transform.localScale *= rightScaleModifier;
+                        float leftScaleModifier = _random.NextFloat(_scaleOffsetMinMax.x, _scaleOffsetMinMax.y);
+                        float rightScaleModifier = _random.NextFloat(_scaleOffsetMinMax.x, _scaleOffsetMinMax.y);
+                        leftObj.transform.localScale *= leftScaleModifier;
+                        rightObj.transform.localScale *= rightScaleModifier;
                                     
-                    _propsSpawn.Add(leftObj);
-                    _propsSpawn.Add(rightObj);
+                        _propsSpawn.Add(leftObj);
+                        _propsSpawn.Add(rightObj);
+                    }
+                    else if (_typeSpawning == TypeSpawningProps.Left || randomSide == 1)
+                    {
+                        Vector3 leftPos = position - right * (_width * 0.5f);
+
+                        leftPos = transform.InverseTransformPoint(leftPos);
+
+                        leftPos += new Vector3(_random.NextFloat(-_positionOffset, _positionOffset) + transform.position.x, _heightOffset, _random.NextFloat(-_positionOffset, _positionOffset) + transform.position.z);
+
+                        if (!_localHeightOffset)
+                        {
+                            leftPos.y = _heightOffset;
+                        }
+                    
+                        GameObject leftObj = Instantiate(_props[_random.NextInt(0, _props.Count)], leftPos, Quaternion.Euler(0, _random.NextFloat(-_rotationOffset, _rotationOffset), 0), _transformParent);
+                
+                        float leftScaleModifier = _random.NextFloat(_scaleOffsetMinMax.x, _scaleOffsetMinMax.y);
+                        leftObj.transform.localScale *= leftScaleModifier;
+                                    
+                        _propsSpawn.Add(leftObj);
+                    }
+                    else if (_typeSpawning == TypeSpawningProps.Right || randomSide == 2)
+                    {
+                        Vector3 rightPos = position + right * (_width * 0.5f);
+
+                        rightPos = transform.InverseTransformPoint(rightPos);
+
+                        rightPos += new Vector3(_random.NextFloat(-_positionOffset, _positionOffset) + transform.position.x, _heightOffset, _random.NextFloat(-_positionOffset, _positionOffset) + transform.position.z);
+
+                        if (!_localHeightOffset)
+                        {
+                            rightPos.y = _heightOffset;
+                        }
+                    
+                        GameObject rightObj = Instantiate(_props[_random.NextInt(0, _props.Count)], rightPos, Quaternion.Euler(0, _random.NextFloat(-_rotationOffset, _rotationOffset), 0), _transformParent);
+                
+                        float rightScaleModifier = _random.NextFloat(_scaleOffsetMinMax.x, _scaleOffsetMinMax.y);
+                        rightObj.transform.localScale *= rightScaleModifier;
+                                    
+                        _propsSpawn.Add(rightObj);
+                    }
                 }
             }
         }
@@ -97,5 +147,13 @@ namespace MapGeneration
         {
             _density = density;
         }
+    }
+
+    public enum TypeSpawningProps
+    {
+        Symmetrical,
+        Left,
+        Right,
+        OneSideRandom
     }
 }
