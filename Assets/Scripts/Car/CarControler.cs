b8308483768d83e70124edd13ce6c452ba1d00e1
Alexandre Renaux <a.renaux@ensi.lan>
2025-12-17 16:55:13 +0100

    added Boost vfx

--- Assets/Scripts/Car/CarControler.cs
+++ Assets/Scripts/Car/CarControler.cs
@@ -46,9 +46,9 @@ namespace Car
         public float particleMinSpeed = 0.5f; // Minimal speed to activate wheel particles
         public float particleMaxEmission = 30f; // Max 
 
-        [Header("Nitro Attack")] 
-        [SerializeField] private GameObject _nitroCollider; // Collider activated during boost for nitro attack
-
+        [Header("Boost VFX")]
+        [SerializeField] private ParticleSystem _boostVFX;
+        
         [Header("SFX")] 
         [SerializeField] private EventReference _engineSound;
         [SerializeField] private EventReference _boostSound;
@@ -260,6 +260,9 @@ namespace Car
         {
             // Play boost sound
             AudioManager.Instance.Play(_boostSound, follow: gameObject);
+            // Play boost VFX
+            if (_boostVFX) _boostVFX.Play();
+            
             
             _nextBoostTime = 0f;
 
